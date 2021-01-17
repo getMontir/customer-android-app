84bfbe4cd6d26c081cb37b92b3ad59c3d9b8f475
Ahmad Faris <isfawwaz@gmail.com>
2021-01-17 19:30:46 +0700

    Setup ui main - vehicle

--- app/src/main/java/com/getmontir/customer/view/ui/main/VehicleFragment.kt
+++ app/src/main/java/com/getmontir/customer/view/ui/main/VehicleFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.getmontir.customer.R
+import com.getmontir.customer.databinding.FragmentMainVehicleBinding
 import com.getmontir.customer.view.ui.base.GetFragment
 
 
@@ -27,11 +30,20 @@ class VehicleFragment : GetFragment() {
         fun newInstance() = VehicleFragment()
     }
 
+    private lateinit var binding: FragmentMainVehicleBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_vehicle, container, false)
+        binding = FragmentMainVehicleBinding.inflate( inflater, container, false )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Setup toolbar
+        binding.toolbar.setupWithNavController( findNavController() )
     }
 }
